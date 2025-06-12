6ec8b8adae73e844d5b3d648fae674d85eb9d5c7
andersondamasio <40115647+andersondamasio@users.noreply.github.com>
2025-06-12 09:07:59 -0300

    Add files via upload

---
+++ MauiAppIOSSampleBitMono/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppIOSSampleBitMono
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
